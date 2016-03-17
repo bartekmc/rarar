099aa5e4605e4bf0be6b813ed0ff3e0c281ecb5e
bartekmc <bartekmc@gmail.com>
2016-03-17 08:27:32 +0100

    u

--- comx/cx.py
+++ comx/cx.py
@@ -77,6 +77,7 @@ class ComicHost(Bottle):
         self.route('/s/r/<path:path>/<page:int>', callback=self.saveX)
 
     def saveX(self, path, page):
+        print "SAVEX:", path, page
         path = urllib.unquote(path)
         f = open(DB_FILE, 'r+')
         flines = f.readlines()
@@ -97,7 +98,7 @@ class ComicHost(Bottle):
         print repr("write:"+ '\n'.join(fout))
         f.seek(0)
         f.truncate()
-        f.write(''.join(fout))
+        f.write('\n'.join(fout))
         f.close()
         
         return template("{{X}} {{P}}", X=path, P=page)
@@ -135,20 +136,12 @@ class ComicHost(Bottle):
 
     def open_file_a(self, path, page):
         hobj = HtmlObj()
-        print "========"
-        print path, path, path
-        print "========"
-
-#        if os.path.isfile(path):
-#            pass
 
         path_cache = os.path.join(CACHE_PATH, path)
         path_file = os.path.join(os.path.abspath(os.curdir), path)
         pages = []
         matches = []
 
-        #jezeli jest cache
-        print "Y:", path_cache
         if not os.path.isdir(path_cache):
             os.makedirs(path_cache)
 
@@ -157,21 +150,12 @@ class ComicHost(Bottle):
 
             rf = rarfile.RarFile(path_file)
             xxfiles = rf.namelist()
-#            for f in rf.namelist():
-#                print f.filename, f.file_size
-#                if f.filename == 'README':
-#                    print(rf.read(f))
 
             t = threading.Thread(target=self.unpack, args=(path_file, path_cache))
-#            threads.append(t)
             t.start()
 
-#            patoolib.extract_archive(path_file, outdir=path_cache)
 
             for f in xxfiles:
-                #matches.append(os.path.relpath(os.path.join(path_cache, f)))
-                #print "XXXXXXXXXXXXXXX:", os.path.join(path_cache, f)
-#                matches.append(os.path.relpath(os.path.join(root, filename),path_cache))
                 matches.append(f)
 
         else:
@@ -180,58 +164,42 @@ class ComicHost(Bottle):
             rf = rarfile.RarFile(path_file)
             xxfiles = rf.namelist()
             for f in xxfiles:
-                #matches.append(os.path.relpath(os.path.join(path_cache, f)))
-                #print "XXXXXXXXXXXXXXX:", os.path.join(path_cache, f)
-#                matches.append(os.path.relpath(os.path.join(root, filename),path_cache))
                 matches.append(f)
-#            for root, dirnames, filenames in os.walk(path_cache):
-#                for filename in filenames:
-#                        matches.append(os.path.relpath(os.path.join(root,
-#                                                                filename),
-#                                                   path_cache))
-
-        print "X:", matches
 
         files = sorted(matches)
         x = int(1)
         for f in files:
-            #p = os.path.join(os.sep, STATIC_CACHE_PREFIX, path, f)
-#            p = os.path.join(os.sep, 'f/', path, f)
             p = os.path.join(os.sep, 'f/', path, 'page', str(x))
             pages.append(p)
             x+=1
-        print pages
 
         back_link = os.path.join('/r/', os.path.split(path)[0])
 
         hobj.dirs = None
         hobj.comics = None
         hobj.image_src = os.path.join(os.sep, STATIC_CACHE_PREFIX, path, files[page])
-        print "000000000"
-        print os.sep, STATIC_CACHE_PREFIX, path, files[page]
-        print hobj.image_src
-        print "000000000"
+
         hobj.pages = pages
-        hobj.prev_page = pages[page-1] 
-        hobj.cur_page = pages[page] 
-        print "1========"
-        print path
-        print "1========"
-#        hobj.next_page = os.path.join('/f/', path, pages[page+1])
-        hobj.next_page = pages[page+1]
+        hobj.prev_page = pages[page-2] 
+        hobj.cur_page = pages[page-1] 
+        hobj.next_page = pages[page]
         hobj.last_page = '2' 
         hobj.back_link = back_link
 
         f = open(DB_FILE, 'r')
         flines = f.readlines()
         for l in flines:
-            #print path, l
             if path in l:
                 hobj.last_page = int(l.split(',')[1])
         f.close()
 
         x = os.path.join('/s/r/', path)
         hobj.saveX_link = urllib.quote(x)
+        if (len(pages)>=page):
+            self.saveX(hobj.saveX_link, 999)
+        else:
+            self.saveX(hobj.saveX_link, page)
+        
         if os.path.isfile(path_file):
             return template(templ, hobj=hobj)
 
@@ -333,14 +301,14 @@ class ComicHost(Bottle):
             last_page = 1
             for lz in flines:
                 if os.path.join(path, f) in lz:
-                    print "lz:", lz
-                    print "lst page:", lz, os.path.join(path, f), int(lz.split(',')[1])
                     last_page = int(lz.split(',')[1])
             fo.close()
             if last_page == 999:
                 i.finished_icon = True 
             if last_page > 1 and last_page != 999:
                 i.read_icon = True
+
+            i.link = os.path.join('/f/', path, f, 'page', str(last_page))
 
         for d in dirsx:
             i = Item()
